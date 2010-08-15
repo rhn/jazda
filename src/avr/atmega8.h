cd6a4b3a8a77a5721779c3e7c0a8e2334fd7c7f4
rhn <gihu.rhn@porcupinefactory.org>
2010-08-15 17:44:28 +0200

    adjusted default pinout to my likings

--- src/avr/atmega8.h
+++ src/avr/atmega8.h
@@ -2,21 +2,21 @@
 #define CLKPORT PORTC
 #define CLKPIN PC5
 
-#define RSTDIR DDRC
-#define RSTPORT PORTC
-#define RSTPIN PC3
-
-#define SCEDIR DDRC
-#define SCEPORT PORTC
-#define SCEPIN PC2
-
 #define SDADIR DDRC
 #define SDAPORT PORTC
 #define SDAPIN PC4
 
 #define D_CDIR DDRC
 #define D_CPORT PORTC
-#define D_CPIN PC1   
+#define D_CPIN PC3
+
+#define SCEDIR DDRC
+#define SCEPORT PORTC
+#define SCEPIN PC2
+
+#define RSTDIR DDRC
+#define RSTPORT PORTC
+#define RSTPIN PC0   
 
 void setup_cpu(void) {
    // makes CPU clock 1 MHz
