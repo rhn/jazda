cd58d0bbf5acac73d398c7960e021675c594aee4
rhn <gihu.rhn@porcupinefactory.org>
2010-08-16 19:20:15 +0200

    added TODOs

--- src/main.c
+++ src/main.c
@@ -69,8 +69,9 @@ TODO
 #endif
 
 #ifdef SPEED_VS_DISTANCE_PLOT
-    // TODO: notofication that CURRENT_SPEED is required
-    // TODO: vertical size. Currently hard-coded to 3 * 8
+    // TODO: compile-time notification that CURRENT_SPEED is required
+    // TODO: vertical size. Currently hard-coded to 2 * 8
+    // TODO: speed axis adjusting to maximum value
     #define SVDPLOT_SIZE 10 // size in pixels (distance axis)
     #define SVDPLOT_LENGTH_KM 1 // distance axis length
 #endif
