c1bd9a82b32e8523f4c5216d2efab7e73350f824
rhn <gihu.rhn@porcupinefactory.org>
2011-06-25 14:04:46 +0200

    fixed avgspeed calculation

--- src/modules/avgspeed.h
+++ src/modules/avgspeed.h
@@ -1,21 +1,18 @@
 /* Average speed module
 REQUIRES: speed, LONG_SPEED
-Doesn't add to average when bicycle is stopped. Average counted from first pulse
-in streak to stopped time.
-TODO: measure from first pulse to last pulse and ignore stray pulses?
+Doesn't add single pulses to average when bicycle is stopped. Average counted
+from first pulse in streak to stopped time.
 */
 
 volatile uint32_t avgspeed_total_time = 0;
 volatile uint16_t avgspeed_pulses = 0;
 
 void avgspeed_on_pulse(void) {
-    if (avgspeed_pulses == 0) {
-        avgspeed_total_time = 0;
-    } else if (oldest_pulse_index) {
+    if (oldest_pulse_index) {
         // pulse index not incremented yet, but data already in the table!
         avgspeed_total_time += pulse_table[0] - pulse_table[2];
+        avgspeed_pulses++;
     }
-    avgspeed_pulses++;
 }
 
 module_actions_t *avgspeed_select(const uint8_t state) {
@@ -33,7 +30,7 @@ void avgspeed_redraw(const uint8_t force) {
     uint32_t speed;
     upoint_t position = {0, 5};
     upoint_t glyph_size = {8, 8};
-    if (avgspeed_pulses > 1) {
+    if (avgspeed_pulses) {
         uint32_t time_difference = avgspeed_total_time;
 //        speed = ((uint32_t)(((uint64_t)SPEED_FACTOR * ((avgspeed_pulses - 1))) >> FRAC_BITS)) / time_difference;
         speed = get_int_average_long(time_difference, avgspeed_pulses);
