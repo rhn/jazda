697666a3eb4976e07ce1db57aecc17a89ed1608d
rhn <gihu.rhn@porcupinefactory.org>
2011-07-27 17:34:54 +0200

    added missing header

---
+++ src/builtins/timer.h
@@ -0,0 +1,32 @@
+/*
+    Copyright 2011 rhn <gihu.rhn@porcupinefactory.org>
+
+    This file is part of Jazda.
+
+    Jazda is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Jazda is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Jazda.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/*
+Timer management, common to all architectures
+*/
+
+#define MAX_TIMERS 7
+
+inline void timer_initialize(void);
+
+inline void timer_dispatch(void);
+
+int8_t timer_set_callback(const uint16_t time, void (*callback)(void));
+
+void timer_clear_callback(const int8_t identifier);
