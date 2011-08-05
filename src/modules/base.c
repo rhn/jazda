0526a6126784da09e697bab3070167834f05f1dc
rhn <gihu.rhn@porcupinefactory.org>
2011-08-05 14:47:38 +0200

    added some references to lib/modules

--- src/modules/base.c
+++ src/modules/base.c
@@ -17,6 +17,8 @@
     along with Jazda.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+#include "../lib/modules.h"
+
 struct module_flags_struct {
     uint8_t distance_changed;
     uint8_t maxspeed_changed;
--- src/modules/svtplot.c
+++ src/modules/svtplot.c
@@ -32,6 +32,7 @@ Possible optimizations: pulse_count starts from -1 and becomes rotation_count
 */
 
 #include "../lib/plots.h"
+#include "../lib/modules.h"
 
 #define SVTPLOT_LENGTH_SEC 84*2 // time axis length.
 #define SVTPLOT_SPEED_TRUNC 5
